c31b88cdf61b6c18368a9922126e540c4fd4d71d
Nguyễn Quỳnh Như <ngquynhnhu1901@gmail.com>
2022-12-05 21:04:21 +0700

    12/5 21:05

--- SEFinalProject_Winform/SEFinalProject_Winform/frmNhapHang.Designer.cs
+++ SEFinalProject_Winform/SEFinalProject_Winform/frmNhapHang.Designer.cs
@@ -37,6 +37,10 @@
             this.label10 = new System.Windows.Forms.Label();
             this.numberProductQuantity = new System.Windows.Forms.NumericUpDown();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.picturePro = new System.Windows.Forms.PictureBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.rtbProDes = new System.Windows.Forms.RichTextBox();
+            this.label11 = new System.Windows.Forms.Label();
             this.tbProPrice = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
             this.tbProUseDate = new System.Windows.Forms.TextBox();
@@ -47,18 +51,16 @@
             this.label6 = new System.Windows.Forms.Label();
             this.tbProOrigin = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.rtbProDes = new System.Windows.Forms.RichTextBox();
-            this.label12 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.btnChooseImage = new System.Windows.Forms.Button();
             this.label13 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnChooseImage = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numberProductQuantity)).BeginInit();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturePro)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -121,15 +123,20 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(915, 127);
+            this.label10.Location = new System.Drawing.Point(883, 128);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(60, 16);
+            this.label10.Size = new System.Drawing.Size(92, 16);
             this.label10.TabIndex = 16;
-            this.label10.Text = "Số lượng";
+            this.label10.Text = "Số lượng thêm";
             // 
             // numberProductQuantity
             // 
             this.numberProductQuantity.Location = new System.Drawing.Point(981, 125);
+            this.numberProductQuantity.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
             this.numberProductQuantity.Name = "numberProductQuantity";
             this.numberProductQuantity.Size = new System.Drawing.Size(53, 20);
             this.numberProductQuantity.TabIndex = 17;
@@ -137,7 +144,7 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.btnChooseImage);
-            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.picturePro);
             this.groupBox1.Controls.Add(this.label12);
             this.groupBox1.Controls.Add(this.rtbProDes);
             this.groupBox1.Controls.Add(this.label11);
@@ -157,6 +164,44 @@
             this.groupBox1.TabIndex = 18;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông tin chi tiết sản phẩm";
+            // 
+            // picturePro
+            // 
+            this.picturePro.BackgroundImage = global::SEFinalProject_Winform.Properties.Resources.icon_noimage;
+            this.picturePro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picturePro.Location = new System.Drawing.Point(902, 19);
+            this.picturePro.Name = "picturePro";
+            this.picturePro.Size = new System.Drawing.Size(117, 134);
+            this.picturePro.TabIndex = 29;
+            this.picturePro.TabStop = false;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(793, 56);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(92, 16);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "Ảnh sản phẩm";
+            // 
+            // rtbProDes
+            // 
+            this.rtbProDes.Location = new System.Drawing.Point(437, 29);
+            this.rtbProDes.Name = "rtbProDes";
+            this.rtbProDes.Size = new System.Drawing.Size(275, 121);
+            this.rtbProDes.TabIndex = 27;
+            this.rtbProDes.Text = "";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(329, 85);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(102, 16);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "Mô tả sản phẩm";
             // 
             // tbProPrice
             // 
@@ -243,57 +288,9 @@
             this.label5.TabIndex = 16;
             this.label5.Text = "Nguồn gốc";
             // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(329, 85);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(102, 16);
-            this.label11.TabIndex = 26;
-            this.label11.Text = "Mô tả sản phẩm";
-            // 
-            // rtbProDes
-            // 
-            this.rtbProDes.Location = new System.Drawing.Point(437, 29);
-            this.rtbProDes.Name = "rtbProDes";
-            this.rtbProDes.Size = new System.Drawing.Size(275, 121);
-            this.rtbProDes.TabIndex = 27;
-            this.rtbProDes.Text = "";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(793, 56);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(92, 16);
-            this.label12.TabIndex = 28;
-            this.label12.Text = "Ảnh sản phẩm";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImage = global::SEFinalProject_Winform.Properties.Resources.icon_noimage;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(902, 19);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(117, 134);
-            this.pictureBox1.TabIndex = 29;
-            this.pictureBox1.TabStop = false;
-            // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "Chọn ảnh của bạn";
-            // 
-            // btnChooseImage
-            // 
-            this.btnChooseImage.Location = new System.Drawing.Point(796, 88);
-            this.btnChooseImage.Name = "btnChooseImage";
-            this.btnChooseImage.Size = new System.Drawing.Size(89, 39);
-            this.btnChooseImage.TabIndex = 30;
-            this.btnChooseImage.Text = "Thay đổi ảnh";
-            this.btnChooseImage.UseVisualStyleBackColor = true;
-            this.btnChooseImage.Click += new System.EventHandler(this.btnChooseImage_Clicked);
             // 
             // label13
             // 
@@ -305,25 +302,64 @@
             this.label13.TabIndex = 19;
             this.label13.Text = "Sản phẩm sẵn có";
             // 
-            // button1
+            // btnAddNew
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(464, 351);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(226, 34);
-            this.button1.TabIndex = 20;
-            this.button1.Text = "Thêm sản phẩm mới";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnAddNew.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAddNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddNew.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAddNew.Location = new System.Drawing.Point(406, 342);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(184, 34);
+            this.btnAddNew.TabIndex = 20;
+            this.btnAddNew.Text = "Thêm sản phẩm mới";
+            this.btnAddNew.UseVisualStyleBackColor = false;
+            this.btnAddNew.Click += new System.EventHandler(this.addNewProduct);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(33, 434);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(1163, 187);
             this.dataGridView1.TabIndex = 21;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.loadCellContent);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.Control;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnClear.Location = new System.Drawing.Point(1026, 342);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(113, 34);
+            this.btnClear.TabIndex = 22;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_clicked);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnUpdate.Location = new System.Drawing.Point(596, 342);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(184, 34);
+            this.btnUpdate.TabIndex = 23;
+            this.btnUpdate.Text = "Cập nhật";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.updateProduct);
+            // 
+            // btnChooseImage
+            // 
+            this.btnChooseImage.Location = new System.Drawing.Point(796, 88);
+            this.btnChooseImage.Name = "btnChooseImage";
+            this.btnChooseImage.Size = new System.Drawing.Size(89, 39);
+            this.btnChooseImage.TabIndex = 30;
+            this.btnChooseImage.Text = "Thêm ảnh";
+            this.btnChooseImage.UseVisualStyleBackColor = true;
+            this.btnChooseImage.Click += new System.EventHandler(this.btnChooseImage_Clicked);
             // 
             // frmNhapHang
             // 
@@ -331,8 +367,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1231, 633);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnClear);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAddNew);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.numberProductQuantity);
@@ -349,7 +387,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.numberProductQuantity)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturePro)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -367,7 +405,7 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.NumericUpDown numberProductQuantity;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picturePro;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.RichTextBox rtbProDes;
         private System.Windows.Forms.Label label11;
@@ -381,10 +419,12 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox tbProOrigin;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button btnChooseImage;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAddNew;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnChooseImage;
     }
 }
--- SEFinalProject_Winform/SEFinalProject_Winform/frmNhapHang.cs
+++ SEFinalProject_Winform/SEFinalProject_Winform/frmNhapHang.cs
@@ -5,17 +5,21 @@ using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace SEFinalProject_Winform
 {
     public partial class frmNhapHang : Form
     {
         String strConn = ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString;
-
+        String proImage = "";
+        int proQuantityNow = 0;
         public frmNhapHang()
         {
             InitializeComponent();
@@ -54,14 +58,185 @@ namespace SEFinalProject_Winform
             if (open.ShowDialog() == DialogResult.OK)
             {
                 // display image in picture box  
-                pictureBox1.Image = new Bitmap(open.FileName);
+                picturePro.Image = new Bitmap(open.FileName);
+                picturePro.SizeMode = PictureBoxSizeMode.StretchImage;
                 // image file path  
                 String image_path = open.FileName;
+                proImage = image_path;
             }
         }
 
         private void frmNhapHang_Load(object sender, EventArgs e)
         {
+            LoadData();
+            btnUpdate.Enabled = false;
+            btnAddNew.Enabled = true;
+            btnChooseImage.Enabled = true;
+            
+        }
+
+        private void loadCellContent(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count != 0)
+            {
+                DataGridViewCellCollection row = dataGridView1.SelectedRows[0].Cells;
+                tbProductID.Text = row[0].Value.ToString();
+                tbProductName.Text = row[1].Value.ToString();
+                tbProPrice.Text = row[2].Value.ToString();
+                tbProOrigin.Text = row[3].Value.ToString();
+                tbProType.Text = row[5].Value.ToString();
+                tbProBrand.Text = row[6].Value.ToString();
+                rtbProDes.Text = row[7].Value.ToString();
+                tbProUseDate.Text = row[8].Value.ToString();
+                proQuantityNow = int.Parse(row[4].Value.ToString());
+
+                String imagePath = "D:\\Homework\\SoftwareEngineering\\CloneSE\\FinalProjectCNPM\\Image\\" + row[10].Value.ToString();
+
+
+                picturePro.ImageLocation = imagePath;
+                picturePro.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+                btnAddNew.Enabled = false;
+                btnUpdate.Enabled = true;
+                btnChooseImage.Enabled = false;
+
+                tbProductID.Enabled = false;
+                tbProductName.Enabled = false;
+                tbProBrand.Enabled = false;
+                tbProOrigin.Enabled = false;
+                tbProPrice.Enabled = false;
+                tbProType.Enabled = false;
+                tbProUseDate.Enabled = false;
+                rtbProDes.Enabled = false;
+
+            }
+        }
+
+        private void btnClear_clicked(object sender, EventArgs e)
+        {
+            clearFormData();
+        }
+
+        private void clearFormData()
+        {
+            btnAddNew.Enabled = true;
+            btnChooseImage.Enabled = true;
+            btnUpdate.Enabled = false;
+
+            tbProductID.Text = "";
+            tbProductName.Text = "";
+            tbProBrand.Text = "";
+            tbProOrigin.Text = "";
+            tbProPrice.Text = "";
+            tbProType.Text = "";
+            tbProUseDate.Text = "";
+            rtbProDes.Text = "";
+            picturePro.ImageLocation = "";
+
+            tbProductID.Enabled = true;
+            tbProductName.Enabled = true;
+            tbProBrand.Enabled = true;
+            tbProOrigin.Enabled = true;
+            tbProPrice.Enabled = true;
+            tbProType.Enabled = true;
+            tbProUseDate.Enabled = true;
+            rtbProDes.Enabled = true;
+        }
+
+        private void addNewProduct(object sender, EventArgs e)
+        {
+            String proID = tbProductID.Text.ToString();
+            String proName = tbProductName.Text.ToString();
+            String proOrigin = tbProOrigin.Text.ToString();
+            String proType = tbProType.Text;
+            String proBrand = tbProBrand.Text;
+            String proDes = rtbProDes.Text;
+            float proPrice = 0;
+            int proQuantity = 0;
+            String useDate = tbProUseDate.Text.ToString();
+            try
+            {
+                proQuantity = int.Parse(numberProductQuantity.Value.ToString());
+                proPrice = float.Parse(tbProPrice.Text.ToString());
+                //useDate = DateTime.ParseExact(tbProUseDate.Text.ToString(), "MM/dd/yyyy", System.Globalization.CultureInfo.CurrentCulture);
+            } catch (Exception)
+            {
+                MessageBox.Show("Có lỗi trong quá trình thêm sản phẩm");
+            }
+
+            //insert into PRODUCT values ('P0000',N'Tên sản phẩm', 0, N'Xuất xứ', 0, N'Type', N'Brand', N'Des', '2024-12-23',0);
+
+            String sSQL = "INSERT INTO PRODUCT (PRODUCTID, PRONAME, PROPRICE, PROORIGIN, PROQUANTITY, PROTYPE ,PROBRAND, PRODESCRIPTION, USEDATE, HASSOLD, PROIMAGE)VALUES (@ProID, @ProName, @ProPrice, @ProOrigin, @ProQuantity, @ProType, @ProBrand, @ProDes, @UseDate, @HasSold, @ProImage)";
+            SqlConnection conn = new SqlConnection(strConn);
+           
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sSQL, conn);
+
+            cmd.Parameters.Add(new SqlParameter("@ProID", proID));
+            cmd.Parameters.Add(new SqlParameter("@ProName",proName));
+            cmd.Parameters.Add(new SqlParameter("@ProPrice", proPrice));
+            cmd.Parameters.Add(new SqlParameter("@ProOrigin", proOrigin));
+            cmd.Parameters.Add(new SqlParameter("@ProType", proType));
+            cmd.Parameters.Add(new SqlParameter("@ProQuantity", proQuantity));
+            cmd.Parameters.Add(new SqlParameter("@ProBrand", proBrand));
+            cmd.Parameters.Add(new SqlParameter("@ProDes", proDes));
+            cmd.Parameters.Add(new SqlParameter("@UseDate", useDate));
+            cmd.Parameters.Add(new SqlParameter("@HasSold", 1));
+            cmd.Parameters.Add(new SqlParameter("@ProImage", proImage));
+
+            try
+            {
+
+                cmd.ExecuteNonQuery();
+                clearFormData();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error:" + ex.Message);
+            }
+            MessageBox.Show("Save successfully!");
+            LoadData();
+
+        }
+
+        private void updateProduct(object sender, EventArgs e)
+        {
+            String proID = tbProductID.Text.ToString();
+
+            int proQuantity = 0;
+            try
+            {
+                proQuantity = int.Parse(numberProductQuantity.Value.ToString());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Có lỗi trong quá trình thêm sản phẩm");
+            }
+
+            proQuantity = proQuantity + proQuantityNow;
+
+            
+            String sSQL = "UPDATE PRODUCT SET PROQUANTITY = @ProQuantity WHERE PRODUCTID = @ProID";
+            SqlConnection conn = new SqlConnection(strConn);
+
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sSQL, conn);
+            //cmd.Parameters.Add(new SqlParameter("@maso", maso));
+            cmd.Parameters.Add(new SqlParameter("@ProID", proID));
+            cmd.Parameters.Add(new SqlParameter("@ProQuantity", proQuantity));
+
+            try
+            {
+
+                cmd.ExecuteNonQuery();
+                clearFormData();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error:" + ex.Message);
+            }
+            MessageBox.Show("Save successfully!");
             LoadData();
         }
     }
